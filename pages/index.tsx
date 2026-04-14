990170f405bc1d03c6df8af7562b0c72230c96c7
Swoyam Siddharth <me@swoyam.in>
2026-04-14 13:39:15 +0530

    fix: Apple ui things

--- pages/index.tsx
+++ pages/index.tsx
@@ -664,6 +664,10 @@ export default function Home() {
             <Head>
                 <title>Finance Tracker</title>
                 <link rel="icon" type="image/png" href="/favicon.png" />
+                <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+                <meta name="apple-mobile-web-app-capable" content="yes" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                <meta name="apple-mobile-web-app-title" content="Finance" />
                 <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
                 <meta name="theme-color" content="#000000" />
             </Head>
